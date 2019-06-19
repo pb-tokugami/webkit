b0fbffbf0896a3b92b8c9c9e81e0337b931adeed
Ryuichi Tokugami <ryuichi.tokugami@plan-b.co.jp>
2019-06-19 11:39:39 +0900

    add iOS 12.3

---
+++ iOS 12.3/WebKit/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  WebKit
+//
+//  Created by 得上竜一 on 2019/06/06.
+//
+
+#include "main.hpp"
+
+
+void main(){
+    
+}
---
+++ iOS 12.3/WebKit/main.hpp
@@ -0,0 +1,13 @@
+//
+//  main.hpp
+//  WebKit
+//
+//  Created by 得上竜一 on 2019/06/06.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
